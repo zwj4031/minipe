1c8c91d8cee76a5de8559a781cc0e44e9288bcc3
zwj4031 <dnyl@qq.com>
2021-05-01 13:16:51 +0800

    update winxshell

--- x64/x/Program Files/wxsUI/UI_Text/UI_Debug.bat
+++
@@ -1,4 +0,0 @@
-cd /d "%~dp0..\.."
-start x64\Debug\WinXShell.exe -console -ui -jcfg wxsUI\UI_Text\main.jcfg
-
-start x64\Debug\WinXShell.exe -console -ui -jcfg wxsUI\UI_Text\main.jcfg -text "得到服务器IP为:<u>127.0.0.1</u>"
